3e01cd334adcec468f31204c4b8e4ffabcd009a4
youjunho613 <njn613@gmail.com>
2024-04-22 12:56:18 +0900

    design. nav fontsize 변경

--- src/app/board/[...category]/page.tsx
+++ src/app/board/[...category]/page.tsx
@@ -23,7 +23,7 @@ export default function Board({ params: { category: params } }: IProps) {
   return (
     <div>
       <nav>
-        <ul className="contents-center relative gap-12 w-[1000px] h-12 mb-12 navigation animation text-sm">
+        <ul className="relative contents-center gap-12 w-[1000px] h-12 mb-12 navigation animation text-lg">
           <li>
             <Link href={`/board/${category.mainCategory}`}>전체</Link>
           </li>
