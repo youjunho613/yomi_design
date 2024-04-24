4691f385db0e34601c78e6716196b5ee3fe01456
youjunho613 <njn613@gmail.com>
2024-04-24 14:56:50 +0900

    design. header 반응형 구현

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -4,8 +4,8 @@ import NavItem from "./NavItem";
 
 export default function Header() {
   return (
-    <header className="contents-between navigation mt-16 w-full rounded-full px-10">
-      <Link href="/">
+    <header className="contents-between navigation min-w-fit">
+      <Link className="min-w-fit" href="/">
         <Image width={125} height={24} src="/logo.svg" priority alt="요미디자인" />
       </Link>
       <NavItem />
