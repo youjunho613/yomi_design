3eb83b05b6f7913d70f5d3971580569a65bc1d12
youjunho613 <njn613@gmail.com>
2024-05-04 01:22:16 +0900

    chore. image 반응형 수정

--- src/components/home/MainVisual.tsx
+++ src/components/home/MainVisual.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function MainVisual() {
   return (
     <div className="contents-between flex-col gap-10 xl:flex-row-reverse">
-      <Image width={353} height={235} src="/home-logo.png" alt="로고" className="w-full" />
+      <Image width={353} height={235} src="/home-logo.png" alt="로고" className="w-full sm:w-auto" />
       <div className="flex flex-col justify-center gap-6">
         <Image width={530} height={143} src="/home-decs.png" alt="아이덴티티와 디자인을 연결하다" className="w-full" />
         <SocialBox />
