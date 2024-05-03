9df50ec0cbd08226f4b6c3997fa279570167a347
youjunho613 <njn613@gmail.com>
2024-05-04 04:52:52 +0900

    fix. admin page 인증 전 ui 수정

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -31,36 +31,38 @@ export default function AdminLayout({ children }: IProps) {
   return (
     <div className="flex flex-col gap-4">
       {user && (
-        <div className="contents-between flex-col gap-4 sm:flex-row">
-          <p>{user.email}</p>
-          <h1>관리자 페이지</h1>
-          <button className="basic-button rounded-2xl px-4 py-3" onClick={logoutHandler}>
-            로그아웃
-          </button>
-        </div>
+        <>
+          <div className="contents-between flex-col gap-4 sm:flex-row">
+            <p>{user.email}</p>
+            <h1>관리자 페이지</h1>
+            <button className="basic-button rounded-2xl px-4 py-3" onClick={logoutHandler}>
+              로그아웃
+            </button>
+          </div>
+          <ul className="tab">
+            <li className="tab-button-box">
+              <Link href={"/admin/estimateList"} className="tab-button">
+                문의
+              </Link>
+            </li>
+            <li className="tab-button-box">
+              <Link href={"/admin/management"} className="tab-button">
+                게시물 관리
+              </Link>
+            </li>
+            <li className="tab-button-box">
+              <Link href={"/admin/create"} className="tab-button">
+                게시물 등록
+              </Link>
+            </li>
+          </ul>
+          {/* <Sidebar /> */}
+          <div className="mb-2 mt-10">
+            {children}
+            {/* <Navbar /> */}
+          </div>
+        </>
       )}
-      <ul className="tab">
-        <li className="tab-button-box">
-          <Link href={"/admin/estimateList"} className="tab-button">
-            문의
-          </Link>
-        </li>
-        <li className="tab-button-box">
-          <Link href={"/admin/management"} className="tab-button">
-            게시물 관리
-          </Link>
-        </li>
-        <li className="tab-button-box">
-          <Link href={"/admin/create"} className="tab-button">
-            게시물 등록
-          </Link>
-        </li>
-      </ul>
-      {/* <Sidebar /> */}
-      <div className="mb-2 mt-10">
-        {children}
-        {/* <Navbar /> */}
-      </div>
     </div>
   );
 }
